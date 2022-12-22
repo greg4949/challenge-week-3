0bed4e4150320aee2b0d1b07ada2aa55fb12618d
WJFil <greg.kitchen7@gmail.com>
2022-12-22 16:59:14 -0700

    Add content to readme file and final polishing changes to js code

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# challenge-week-3
-Bootcamp week 3 challenge
+# Weekly Challenge 03 Create Password Generator Using Javascript
+
+![](./passwordgen.jpg)
+
+
+## URL to Portfolio
+
+https://greg4949.github.io/challenge-week-3/
+
+## Description
+
+The purpose of this project was to create a web based random password generator using javascript.
+
+## Installation
+
+N/A
+
+## Usage
+
+Password generator can be accessed at the URL listed at the top of the README.  Clicking the generate password button will trigger a series of prompts for user to define their password criteria.
+Once prompts have received input from user, the randomly generated password will display the password in a text box for the user to be able to see.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-// Assignment Code
+//assign variable to generate password
 var generateBtn = document.querySelector("#generate");
 
+//add event listener to "Generate Password" button so that password generation process starts when clicked
 generateBtn.addEventListener("click", generatePassword);
 
 function generatePassword() {
-  var minlength=8;
-  var maxlength=128;
-  //promt user for length of password between 8 and 128 characters
-  var pwordlength = window.prompt("Enter Password Length between " +minlength + " and "+ maxlength);
+      
+      //define min and max lengths for password
+      var minlength=8;
+      var maxlength=128;
+      
+      //promt user for length of password between 8 and 128 characters
+      var pwordlength = window.prompt("Enter Password Length between " +minlength + " and "+ maxlength);
   
-  
-  if(pwordlength<minlength || pwordlength>maxlength) {
-    window.alert("Selected password length does not fall between 8 and 128 characters.  Please try again")
-  } else {
-      //assign variables for password character criteria
+    //check if password length falls betweeen stated parameters.  End if not and have user try again
 
+    if(pwordlength<minlength || pwordlength>maxlength) {
+      window.alert("Selected password length does not fall between 8 and 128 characters.  Please try again")
+    } else {
+        
+      //assign variables for different categories of password characters
+            var lowercase="abcdefghijklmnopqrstuvwxyz";
+            var uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numbers="0123456789";
+            var special="!@#$%^&*()";
 
-          var lowercase="abcdefghijklmnopqrstuvwxyz";
-          var uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          var numbers="0123456789";
-          var special="!@#$%^&*()";
+      //have user confirm whether or not to use the available character categories
+            var lcaseanswer = window.confirm("Use lowercase letters?  Choose OK for yes and Cancel for No");
+            var ucaseanswer =  window.confirm("Use uppercase letters? Choose OK for yes and Cancel for No");
+            var numbanswer =  window.confirm("Use numbers? Choose OK for yes and Cancel for No");
+            var specanswer =  window.confirm("Use special characters? Choose OK for yes and Cancel for No");
+            
+      //set initial values of password character array and final password to be empty      
+            var pwordchar="";
+            var password="";
 
-//have user confirm whether or not to use these character types
-          var lcaseanswer = window.confirm("Use lowercase letters?  Choose OK for yes and Cancel for No");
-          var ucaseanswer =  window.confirm("Use uppercase letters? Choose OK for yes and Cancel for No");
-          var numbanswer =  window.confirm("Use numbers? Choose OK for yes and Cancel for No");
-          var specanswer =  window.confirm("Use special characters? Choose OK for yes and Cancel for No");
-          var pwordchar="";
-          var password="";
-
-
-
-          //create variable to house characters based on prompt entry
-
+    //generate array of possible characters for password generator to select from
             if(lcaseanswer===true && ucaseanswer!==true && numbanswer !==true && specanswer !==true ) {
               pwordchar=lowercase
           } else if (lcaseanswer!==true && ucaseanswer===true && numbanswer !==true && specanswer !==true ) {
@@ -64,12 +67,13 @@ function generatePassword() {
           } else if (lcaseanswer===true && ucaseanswer===true && numbanswer ===true && specanswer ===true ) {
             pwordchar=lowercase+uppercase+numbers+special
           } 
-
+      //Randomly select characters from available character array to form password.
           for (let i=0; i<pwordlength; i++) {
-            randomnumber=Math.floor(Math.random()*pwordchar.length)
-            password+=pwordchar.substring(randomnumber,randomnumber+1)
+            randomnumber=Math.floor(Math.random()*pwordchar.length);
+            password+=pwordchar.substring(randomnumber,randomnumber+1);
           } 
-
+      
+      //write password to password text box for user to see
           document.getElementById("password").innerHTML=password;
 
           }
