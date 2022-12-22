281a37e56a349546a5b1619268a52071f7856c18
WJFil <greg.kitchen7@gmail.com>
2022-12-21 23:19:21 -0700

    create generate password function and declare variables inside fuction

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
 
-  passwordText.value = password;
+//assign variables for password character criteria
+
+var minlength=8;
+var maxlength=128;
+var lowercase="abcdefghijklmnopqrstuvwxyz";
+var uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers="0123456789";
+var special="!@#$%^&*()";
+
+
+//promt user for length of password between 8 and 128 characters
+var pwordlength = window.prompt("Enter Password Length between " +minlength + " and "+ maxlength);
+//have user confirm whether or not to use these character types
+var lcaseanswer = window.confirm("Use lowercase letters?  Choose OK for yes and Cancel for No");
+var ucaseanswer =  window.confirm("Use uppercase letters? Choose OK for yes and Cancel for No");
+var numbanswer =  window.confirm("Use numbers? Choose OK for yes and Cancel for No");
+var specanswer =  window.confirm("Use special characters? Choose OK for yes and Cancel for No");
+var pwordchar="";
+var password="";
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
